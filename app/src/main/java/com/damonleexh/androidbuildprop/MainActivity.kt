44581c80997cade2f2155539223b589cce9110a9
damonleexh <damon_lee@aliyun.com>
2020-01-07 13:39:22 +0800

    权限请求优化

--- app/src/main/java/com/damonleexh/androidbuildprop/MainActivity.kt
+++ app/src/main/java/com/damonleexh/androidbuildprop/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.damonleexh.androidbuildprop
 
+import android.app.AlertDialog
 import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
 import android.view.KeyEvent
 import android.widget.ArrayAdapter
 import android.widget.BaseAdapter
@@ -16,6 +20,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
     private val PERMISSION = Permission.READ_PHONE_STATE
+    private val REQUEST_PERMISSION_CODE = 999
 
     private fun hasPermission(context: Context): Boolean {
         val perm: Int = context.checkCallingOrSelfPermission(PERMISSION)
@@ -44,8 +49,42 @@ class MainActivity : AppCompatActivity() {
             .runtime()
             .permission(PERMISSION)
             .onGranted { requestBuildProp() }
-            .onDenied { requestPermission() }
+            .onDenied {
+                if (AndPermission.hasAlwaysDeniedPermission(this, PERMISSION)) {
+                    go2System()
+                } else {
+                    requestPermission()
+                }
+            }
             .start()
+    }
+
+    protected fun go2System() {
+        AlertDialog.Builder(this)
+            .setMessage("需要手动开启权限才能使用")
+            .setPositiveButton("设置") { _, _ ->
+                // 跳转到app设置
+                val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                intent.data = Uri.parse("package:$packageName")
+                startActivityForResult(intent, REQUEST_PERMISSION_CODE)
+            }
+            .setNegativeButton("取消") { _, _ ->
+                Toast.makeText(this, "缺失必要权限导致部分功能无法使用", Toast.LENGTH_SHORT).show()
+                finish()
+            }
+            .create()
+            .show()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (RESULT_OK == resultCode) {
+            if (REQUEST_PERMISSION_CODE == requestCode) {
+                requestBuildProp()
+                return
+            }
+        }
+        go2System()
     }
 
     private fun requestBuildProp() {
